290aeaa7741f1ea34c4183e13eaba9484992b33c
ds49289 <dorotea.simunovic@fer.hr>
2019-12-09 13:03:18 +0100

    Networking

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,37 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach(var component in componentsToDisable)
+            {
+                component.enabled = false;
+            }
+        } else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
+    void OnDisable()
+    {
+        if(sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
