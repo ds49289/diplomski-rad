f5d1be0167d9577630d5a0efd3e95ab333503977
Dominik Macek <dominik.macek@3mi-lab.hr>
2020-01-04 12:54:58 +0100

    added shield raise on right click

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private GameObject wand;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    void Start()
+    {
+        if (wand == null)
+        {
+            Debug.LogError("PlayerShoot: No wand referenced!");
+            this.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit _hit;
+        
+        // We hit something
+        if (Physics.Raycast(wand.transform.position, wand.transform.up, out _hit, weapon.range, mask))
+        {
+            Debug.Log("We hit " + _hit.collider.name);
+        }
+    }
+}
---
+++ Assets/Scripts/ShieldManager.cs
@@ -0,0 +1,106 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldManager : NetworkBehaviour
+{
+
+    [SerializeField]
+    private GameObject shield;
+
+    private bool isShieldUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shield.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if(Input.GetButton("Fire2"))
+        {
+            if (!isShieldUp)
+            {
+                ActivateShield();
+                if(isServer)
+                {
+                    RpcActivateShield();
+                }
+                else
+                {
+                    CmdActivateShield();
+                }
+            }
+        }
+        else
+        {
+            if (isShieldUp)
+            {
+                DisableShield();
+                if (isServer)
+                {
+                    RpcDisableShield();
+                }
+                else
+                {
+                    CmdDisableShield();
+                }
+            }
+        }
+
+    }
+
+    public void ActivateShield()
+    {
+        shield.SetActive(true);
+        isShieldUp = true;
+    }
+
+    public void DisableShield()
+    {
+        isShieldUp = false;
+        shield.SetActive(false);
+    }
+
+    [Command]
+    void CmdActivateShield()
+    {
+        ActivateShield();
+        RpcActivateShield();
+    }
+
+    [ClientRpc]
+    void RpcActivateShield()
+    {
+        if(isLocalPlayer)
+        {
+            return;
+        }
+        ActivateShield();
+    }
+
+    [Command]
+    void CmdDisableShield()
+    {
+        ActivateShield();
+        RpcDisableShield();
+    }
+
+    [ClientRpc]
+    void RpcDisableShield()
+    {
+        if (isLocalPlayer)
+        {
+            return;
+        }
+        DisableShield();
+    }
+}
